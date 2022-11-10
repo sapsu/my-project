fcdaae45a63224bcb9e51cf2bccfba13782f9096
sapsu <113932803+sapsu@users.noreply.github.com>
2022-11-10 16:02:50 +0100

    Added insights for different blood pressure levels

--- script.js
+++ script.js
@@ -30,6 +30,18 @@ function calculateBloodPressure(event) {
 
   let result = document.querySelector("#result");
   result.innerHTML = `Your average blood pressure is ${averagesys} / ${averagedias} mmHg.`;
+
+  let insight = document.querySelector("#insight");
+
+  if (averagesys < 130 && averagedias < 80) {
+    insight.innerHTML = `Your blood pressure is normal. Keep up the good lifestyle choices.`;
+  } else if (averagesys < 150 && averagedias < 90) {
+    insight.innerHTML = `Your blood pressure is somewhat elevated. You should consider your lifestyle choices to get it under control. For example, too high salt intake, stress, and alcohol consumption and poor sleep quality can raise your blood pressure. Exercising regularly is good for your crdiovascular health.`;
+  } else if (averagesys < 160 && averagedias < 100) {
+    insight.innerHTML = `Your blood pressure is significantly elevated. You should consult with a doctor within the next month.`;
+  } else {
+    insight.innerHTML = `Your blood pressure is dangerously elevated. You should consult with a doctor immediately.`;
+  }
 }
 
 let calculate = document.querySelector("#blood-pressure-form");
