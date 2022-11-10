a1950a9a3b19227ec82df7ea913ae1c6fa3394f0
sapsu <113932803+sapsu@users.noreply.github.com>
2022-11-10 14:42:45 +0100

    Initial commit
    Started work on blood pressure chart. Added html, css and js files.

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("hello");
+function calculateBloodPressure(event) {
+  event.preventDefault();
+  let sys1 = document.querySelector("#sys1");
+  let sys2 = document.querySelector("#sys2");
+  let dias1 = document.querySelector("#dias1");
+  let dias2 = document.querySelector("#dias2");
+
+  console.log(sys1.value);
+  console.log(sys2.value);
+  console.log(dias1.value);
+  console.log(dias2.value);
+  let averagesys = Math.round((sys1.valueAsNumber + sys2.valueAsNumber) / 2);
+  let averagedias = Math.round((dias1.valueAsNumber + dias2.valueAsNumber) / 2);
+
+  let result = document.querySelector("#result");
+  result.innerHTML = `Your average blood pressure is ${averagesys} / ${averagedias}.`;
+}
+
+let calculate = document.querySelector("#blood-pressure-form");
+
+calculate.addEventListener("submit", calculateBloodPressure);
